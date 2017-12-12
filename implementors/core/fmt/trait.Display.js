c17757eb5db667b73f7adba035e1e18d6c5c9583
Travis CI User <travis@example.org>
2017-12-12 17:41:38 +0000

    Documentation for alexcrichton/rustc-demangle@48646c6

---
+++ implementors/core/fmt/trait.Display.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["rustc_demangle"] = ["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"rustc_demangle/struct.Demangle.html\" title=\"struct rustc_demangle::Demangle\">Demangle</a>&lt;'a&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
